56459e83fc9e85b505d066f405f14757a2e235bf
Alex <alex@orion.sh>
2018-04-18 11:51:46 +0200

    heroes choose page

---
+++ app/pages/choose.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    Choose your hero
+    <nav>
+      <a href="#" @click.prevent="current = 0">DC</a>
+      <a href="#" @click.prevent="current = 1">Marvel</a>
+    </nav>
+    <ul class="List">
+      <li class="List__Item" @click="select" v-for="(h, i) in heroes" :key="i" v-show="(current && i > 5) || (!current && i < 6)">
+        <img :src="`/${i}.jpg`" alt="hero"/>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      current: 0,
+      heroes: [
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {}
+      ]
+    }
+  },
+  methods: {
+    select () {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.List {
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  margin: 0;
+  padding: 0;
+  &__Item {
+    width: 33%;
+    cursor: pointer;
+    img {
+      display: inline-block;
+      width: 100%;
+    }
+  }
+}
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,64 +1,18 @@
 <template>
   <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        heroes
-      </h1>
-      <h2 class="subtitle">
-        Marvel vs DC Heroes
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
-    </div>
+    Heroes Ranking
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
   async asyncData({ app }) {
     const data = await app.$axios.$get('/_')
 
     console.log(data)
-  },
-  components: {
-    Logo
   }
 }
 </script>
 
-<style>
-.container
-{
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-.title
-{
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-.subtitle
-{
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-.links
-{
-  padding-top: 15px;
-}
+<style lang="scss">
 </style>

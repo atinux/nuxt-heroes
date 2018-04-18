d82de3cd750d0fa4557e891bf2c83fb74f3cb1e6
Sébastien Chopin <seb@chopin.io>
2018-04-18 15:16:51 +0200

    api: Send back full user

--- api/src/users/users.routes.js
+++ api/src/users/users.routes.js
@@ -1,10 +1,17 @@
+const Users = require('./users.service')
+const { HttpError } = require('@terrajs/mono')
+
 module.exports = [
 	{
 		method: 'GET',
 		path: '/user',
     session: true,
-    handler(req, res) {
-      res.json(req.session)
+    async handler(req, res) {
+      const user = await Users.getById(req.session.userId)
+
+      if (!user) throw new HttpError('user-not-found', 404)
+
+      res.json(user)
     }
 	}
 ]

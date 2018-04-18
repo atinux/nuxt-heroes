b8351cf036e0c62c309282e828d02aea53761af3
Sébastien Chopin <seb@chopin.io>
2018-04-19 00:37:05 +0200

    app: No need to authenticate sockets

--- app/plugins/sockets.js
+++ app/plugins/sockets.js
@@ -2,15 +2,7 @@ import io from 'socket.io-client'
 
 export default ({ store, env }, inject) => {
   const socket = io(`${env.api}/push`)
-  inject('socket', socket)
 
-  if (store.getters['auth/connected']) {
-    socket.on('connect', function () {
-      socket
-        .emit('authenticate', { token: store.state.auth.jwt })
-        .on('authenticated', function () {
-          store.dispatch('heroes/syncHeroes')
-        })
-    })
-  }
+  inject('socket', socket)
+  socket.on('connect', () => store.dispatch('heroes/syncHeroes'))
 }
